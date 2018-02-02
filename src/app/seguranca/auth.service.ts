686303f6ecb848415c63023d12c277fd562bf156
mauricio-melo <mauriciomelo.contato@outlook.com>
2018-02-02 16:17:02 -0200

    Adicionando o Access Token nas chamadas HTTP

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -2,7 +2,6 @@ import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import { JwtHelper } from 'angular2-jwt';
-
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -28,11 +27,18 @@ export class AuthService {
     return this.http.post(this.oauthTokenUrl, body, { headers })
       .toPromise()
       .then(response => {
-        console.log(response);
         this.armazenarToken(response.json().access_token);
       })
       .catch(response => {
-        console.log(response);
+        if (response.status === 400) {
+          const responseJson = response.json();
+
+          if (responseJson.error === 'invalid_grant') {
+            return Promise.reject('Usuário ou senha inválida!');
+          }
+        }
+
+        return Promise.reject(response);
       });
   }
 
